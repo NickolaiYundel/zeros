6a6be01be16c62cdaf614af5855b5122ec2aa7d4
qweer <stalkeerr44@gmail.com>
2019-10-13 20:02:12 +0300

    implement_zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let array = expression.split("*");
+  let fac = [], doubleFac = [];
+  let limit;
+  let zeross = 0;
+  let multiplication = valueFact = factor = BigInt(1);
+  for (let i = 0; i < array.length;) {
+    if (array[i].endsWith('!!')) {
+      doubleFac.push(array[i].slice(0, -2));
+    } else {
+      fac.push(array[i].slice(0, -1));
+    }
+    i++
+  }
+  for (let i = 0; i < fac.length;) {
+    limit = Number(fac[i]);
+    for (let i = 0; i < limit;) {
+      valueFact *= BigInt(factor);
+      i++;
+      factor = BigInt(factor) + BigInt(1);
+    };
+    i++
+    multiplication *= valueFact;
+    factor = valueFact = BigInt(1);
+  }
+
+  for (let i = 0; i < doubleFac.length;) {
+    limit = Number(doubleFac[i]);
+    if ((limit % 2) === 1) {
+      for (let i = 0; i < limit;) {
+        valueFact *= BigInt(factor);
+        i = i + 2;
+        factor = BigInt(factor) + BigInt(2);
+      };
+      i++
+      multiplication *= valueFact;
+      factor = valueFact = BigInt(1);
+    }
+
+    else {
+      factor = BigInt(2);
+      for (let i = 0; i < limit;) {
+        valueFact *= BigInt(factor);
+        i = i + 2;
+        factor = BigInt(factor) + BigInt(2);
+      };
+      i++
+      multiplication *= valueFact;
+      factor = valueFact = BigInt(1);
+    }
+  }
+
+  while (multiplication % BigInt(10) == 0) {
+    multiplication = multiplication / BigInt(10);
+    zeross++;
+
+  }
+  return (zeross);
 }
